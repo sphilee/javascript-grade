6d0449a3f4eb2176fefc9f5ddbbfe83a8699669c
DESKTOP-MVLSK2J\Tim <sphilee@ajou.ac.kr>
2017-10-18 13:46:04 +0900

    클린 코드

--- grades.js
+++ grades.js
@@ -1,12 +1,26 @@
 "use strict";
 
-const readline = require('readline');
-const rl = readline.createInterface({
+var readline = require('readline');
+var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let data = [{
+function setGrade() {
+    rl.question('과목을 JSON형태로 입력하세요<종료는 end입력> : ', function (answer) {
+        if (answer === "end") {
+            setTimeout(function () {
+                printScore(data);
+            }, 2000);
+            rl.close(answer);
+        } else {
+            data.push(JSON.parse(answer));
+            setGrade();
+        }
+    });
+};
+
+var data = [{
     'name': '데이터베이스',
     'grade': 'A',
     'credit': 3,
@@ -23,55 +37,9 @@ let data = [{
     'bMajor': false
 }];
 
-function setGrade() {
-    rl.question('과목을 JSON형태로 입력하세요<종료는 end입력> : ', (answer) => {
-        if (answer === "end") {
-            setTimeout(function () {
-                printScore(data);
-            }, 2000);
-            rl.close(answer);
-        } else {
-            data.push(JSON.parse(answer));
-            setGrade();
-        }
-    });
-};
-
-function getGrade(grade) {
-    switch (grade) {
-        case "A+":
-            return 4.5;
-            break;
-        case "A":
-            return 4;
-            break;
-        case "B+":
-            return 3.5;
-            break;
-        case "B":
-            return 3;
-            break;
-        case "C+":
-            return 2.5;
-            break;
-        case "C":
-            return 2;
-            break;
-        case "D+":
-            return 1.5;
-            break;
-        case "D":
-            return 1;
-            break;
-        case "F":
-            return 0;
-            break;
-        default:
-            break;
-    }
-};
-
 function printScore(data) {
+    var totalRating = 0;
+    var majorRating = 0;
     var total = data.reduce(function (a, b) {
         return {
             credit: a.credit + b.credit
@@ -84,15 +52,25 @@ function printScore(data) {
             credit: a.credit + b.credit
         };
     });
-    var totalRating = 0;
-    var majorRating = 0;
     data.forEach(function (out) {
         totalRating += getGrade(out.grade) * (out.credit / total.credit);
         majorRating += out.bMajor ? getGrade(out.grade) * (out.credit / bMajor.credit) : 0;
     });
     console.log("총 평점 : " + totalRating.toFixed(2) + ", 전공평점 : " + majorRating.toFixed(2) + ", 이수학점 : " + total.credit + ", 전공이수학점 : " + bMajor.credit);
-    console.log("4.0학점으로 변환하는 경우 총평점은 " + (totalRating * (4.0 / 4.5)).toFixed(2) + "은 입니다.");
+    console.log("4.0학점으로 변환하는 경우 총평점은 " + (totalRating * (4 / 4.5)).toFixed(2) + "은 입니다.");
 };
 
+function getGrade(grade) {
+    var gradeMap = {
+        A: 4,
+        B: 3,
+        C: 2,
+        D: 1,
+        F: 0
+    };
+    var gradeScore = grade.length > 1 ? gradeMap[grade[0]] + parseFloat(grade[1] + "0.5") : gradeMap[grade];
+
+    return gradeScore;
+}
 
 setGrade();
